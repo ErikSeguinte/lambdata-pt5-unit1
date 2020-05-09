e4c4791b46f44fd614ad306924efcbf473100479
Erik Seguinte <erikseguinte@me.com>
2020-05-08 18:36:40 -0700

    Uses a class

--- lambdata_pt5_primefactorx01/lambdata_pt5_primefactorx01.py
+++ lambdata_pt5_primefactorx01/lambdata_pt5_primefactorx01.py
@@ -5,99 +5,100 @@ from pathlib import Path
 from sklearn import metrics
 
 
-def print_nulls(df: pd.DataFrame):
-    number_of_nulls = df.isna().sum()
-    print(f"Nulls:\n{number_of_nulls}")
+class Helper:
 
+    STATES = {
+        "AL": "Alabama",
+        "AK": "Alaska",
+        "AS": "American Samoa",
+        "AZ": "Arizona",
+        "AR": "Arkansas",
+        "CA": "California",
+        "CO": "Colorado",
+        "CT": "Connecticut",
+        "DE": "Delaware",
+        "DC": "District Of Columbia",
+        "FM": "Federated States Of Micronesia",
+        "FL": "Florida",
+        "GA": "Georgia",
+        "GU": "Guam",
+        "HI": "Hawaii",
+        "ID": "Idaho",
+        "IL": "Illinois",
+        "IN": "Indiana",
+        "IA": "Iowa",
+        "KS": "Kansas",
+        "KY": "Kentucky",
+        "LA": "Louisiana",
+        "ME": "Maine",
+        "MH": "Marshall Islands",
+        "MD": "Maryland",
+        "MA": "Massachusetts",
+        "MI": "Michigan",
+        "MN": "Minnesota",
+        "MS": "Mississippi",
+        "MO": "Missouri",
+        "MT": "Montana",
+        "NE": "Nebraska",
+        "NV": "Nevada",
+        "NH": "New Hampshire",
+        "NJ": "New Jersey",
+        "NM": "New Mexico",
+        "NY": "New York",
+        "NC": "North Carolina",
+        "ND": "North Dakota",
+        "MP": "Northern Mariana Islands",
+        "OH": "Ohio",
+        "OK": "Oklahoma",
+        "OR": "Oregon",
+        "PW": "Palau",
+        "PA": "Pennsylvania",
+        "PR": "Puerto Rico",
+        "RI": "Rhode Island",
+        "SC": "South Carolina",
+        "SD": "South Dakota",
+        "TN": "Tennessee",
+        "TX": "Texas",
+        "UT": "Utah",
+        "VT": "Vermont",
+        "VI": "Virgin Islands",
+        "VA": "Virginia",
+        "WA": "Washington",
+        "WV": "West Virginia",
+        "WI": "Wisconsin",
+        "WY": "Wyoming",
+    }
 
-def predict(X: pd.DataFrame):
-    length = X.shape[0]
-    return np.random.choice([True, False], length)
+    @staticmethod
+    def print_nulls(df: pd.DataFrame):
+        number_of_nulls = df.isna().sum()
+        print(f"Nulls:\n{number_of_nulls}")
 
+    @staticmethod
+    def predict(X: pd.DataFrame):
+        length = X.shape[0]
+        return np.random.choice([True, False], length)
 
-def print_confusion_matrix(y, y_pred):
-    df = pd.DataFrame(
-        metrics.confusion_matrix(y, y_pred),
-        columns=["positive", "negative"],
-        index=["True", "False"],
-    )
-    print(df)
+    @staticmethod
+    def print_confusion_matrix(y, y_pred):
+        df = pd.DataFrame(
+            metrics.confusion_matrix(y, y_pred),
+            columns=["positive", "negative"],
+            index=["True", "False"],
+        )
+        print(df)
 
+    @staticmethod
+    def convert_states(s: pd.Series):
+        """ Converts df['state'] from abbrev to full"""
 
-STATE_PATH = Path("states.json")
-STATES = {
-    "AL": "Alabama",
-    "AK": "Alaska",
-    "AS": "American Samoa",
-    "AZ": "Arizona",
-    "AR": "Arkansas",
-    "CA": "California",
-    "CO": "Colorado",
-    "CT": "Connecticut",
-    "DE": "Delaware",
-    "DC": "District Of Columbia",
-    "FM": "Federated States Of Micronesia",
-    "FL": "Florida",
-    "GA": "Georgia",
-    "GU": "Guam",
-    "HI": "Hawaii",
-    "ID": "Idaho",
-    "IL": "Illinois",
-    "IN": "Indiana",
-    "IA": "Iowa",
-    "KS": "Kansas",
-    "KY": "Kentucky",
-    "LA": "Louisiana",
-    "ME": "Maine",
-    "MH": "Marshall Islands",
-    "MD": "Maryland",
-    "MA": "Massachusetts",
-    "MI": "Michigan",
-    "MN": "Minnesota",
-    "MS": "Mississippi",
-    "MO": "Missouri",
-    "MT": "Montana",
-    "NE": "Nebraska",
-    "NV": "Nevada",
-    "NH": "New Hampshire",
-    "NJ": "New Jersey",
-    "NM": "New Mexico",
-    "NY": "New York",
-    "NC": "North Carolina",
-    "ND": "North Dakota",
-    "MP": "Northern Mariana Islands",
-    "OH": "Ohio",
-    "OK": "Oklahoma",
-    "OR": "Oregon",
-    "PW": "Palau",
-    "PA": "Pennsylvania",
-    "PR": "Puerto Rico",
-    "RI": "Rhode Island",
-    "SC": "South Carolina",
-    "SD": "South Dakota",
-    "TN": "Tennessee",
-    "TX": "Texas",
-    "UT": "Utah",
-    "VT": "Vermont",
-    "VI": "Virgin Islands",
-    "VA": "Virginia",
-    "WA": "Washington",
-    "WV": "West Virginia",
-    "WI": "Wisconsin",
-    "WY": "Wyoming",
-}
-
-
-def convert_states(s: pd.Series):
-    """ Converts df['state'] from abbrev to full"""
-
-    return s.map(STATES)
+        return s.map(Helper.STATES)
 
 
 if __name__ == "__main__":
     X, y = np.arange(10).reshape((5, 2)), [True, False, True, False, True]
-    pred = predict(X)
-    print_confusion_matrix(y, pred)
+    pred = Helper.predict(X)
+    Helper.print_confusion_matrix(y, pred)
     states = pd.Series(["CA", "TX", "ND"])
 
-    print(convert_states(states))
+    print(Helper.convert_states(states))
